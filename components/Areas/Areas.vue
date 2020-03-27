c44c485499a875e33003c51c1f90560b137d8068
Sarim Abbas <sarim.stays@gmail.com>
2020-03-27 00:08:06 -0400

    mobile fix

--- components/Areas/Areas.vue
+++ components/Areas/Areas.vue
@@ -8,26 +8,25 @@ export default {
 </script>
 
 <template>
-  <section
-    id="research"
-    class="flex flex-wrap items-center px-6 py-16 bg-gray-100 sectionFull md:px-6 lg:px-16 lg:px-32"
-  >
-    <div>
-      <!-- text -->
-      <div class="flex flex-wrap justify-center pb-16">
-        <h1
-          class="pb-4 text-4xl font-semibold leading-tight tracking-tight text-gray-800"
-        >Areas of research</h1>
-        <p
-          class="text-xl md:px-32 lg:px-40"
-        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, autem, dolorum, repellendus dolore quae eaque vitae dolorem nihil minus consectetur blanditiis alias cumque quos asperiores provident? Ea fugiat corrupti sapiente?</p>
-      </div>
-      <!-- cards -->
-      <div class="card-grid">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+  <section id="research" class="px-6 py-16 bg-gray-100 screenFull lg:px-32">
+    <div class="flex flex-wrap items-center h-full">
+      <div>
+        <!-- text -->
+        <div class="flex flex-wrap justify-center pb-16">
+          <h1
+            class="pb-4 text-4xl font-semibold leading-tight tracking-tight text-gray-800"
+          >Areas of research</h1>
+          <p
+            class="text-xl md:px-32 lg:px-40"
+          >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, autem, dolorum, repellendus dolore quae eaque vitae dolorem nihil minus consectetur blanditiis alias cumque quos asperiores provident? Ea fugiat corrupti sapiente?</p>
+        </div>
+        <!-- cards -->
+        <div class="card-grid">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
   </section>
